a53487c1791fad6220eafb41156ac15f33434a81
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-06 21:39:02 +0200

    CoffeeMachine 2-nd stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -7,3 +7,9 @@ class CoffeeMachine:
         print("Pouring coffee into the cup")
         print("Pouring some milk into the cup")
         print("Coffee is ready!")
+
+    def calc_amount_of_ingredients(self, num_of_cups):
+        print(f"For {num_of_cups} cups of coffee you will need:")
+        print(f"{200 * num_of_cups} ml of water")
+        print(f"{50 * num_of_cups} ml of milk")
+        print(f"{15 * num_of_cups} g of coffee beans")
--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -3,7 +3,9 @@ from CoffeeMachine import CoffeeMachine
 
 def main():
     coffee_machine = CoffeeMachine()
-    coffee_machine.make_coffee()
+    print("Write how many cups of coffee you will need:")
+    num_of_cups = int(input())
+    coffee_machine.calc_amount_of_ingredients(num_of_cups)
 
 
 if __name__ == '__main__':
