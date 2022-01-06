1fc3ee7ef2476b8b7305380c2a7fec52c297fb79
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-06 19:08:19 +0200

    ChatBot 5-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -17,4 +17,18 @@ print("Now I will prove to you that I can count to any number you want.")
 num = int(input())
 for i in range(num + 1):
     print(i, "!")
-print("Completed, have a nice day!")
+
+print("Let's test your programming knowledge.")
+print("""Why do we use methods? 
+1. To repeat a statement multiple times. 
+2. To decompose a program into several small subroutines. 
+3. To determine the execution time of a program. 
+4. To interrupt the execution of a program.""")
+while True:
+    option = int(input())
+    if option != 2:
+        print("Please, try again.")
+    else:
+        print("Completed, have a nice day!")
+        break
+print("Congratulations, have a nice day!")
