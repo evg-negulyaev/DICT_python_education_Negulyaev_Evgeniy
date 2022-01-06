6da2b16dc7ab5a6abb9b7b715bc47d0e0559304f
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-06 19:51:46 +0200

    Hangman 5-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,8 +1,14 @@
 from random import randint
 
 
-def hint(word):
-    return word[0:3] + "-" * (len(word) - 3)
+def hint(word, guessed_letters):
+    result = ""
+    for letter in word:
+        if letter in guessed_letters:
+            result += letter
+        else:
+            result += '-'
+    return result
 
 
 def random_word(words):
@@ -14,12 +20,18 @@ def main():
 
     words = ['python', 'java', 'javascript', 'php']
     word_to_guess = random_word(words)
-    print(f"Guess the word {hint(word_to_guess)}: ", end="")
-    word = input()
-    if word_to_guess == word:
-        print("You survived!")
-    else:
-        print("You lost!")
+    guessed_letters = set()
+    for _ in range(8):
+        print()
+        print(hint(word_to_guess, guessed_letters))
+        print("Input a letter: ", end='')
+        letter = input()
+        if letter in word_to_guess:
+            guessed_letters.add(letter)
+        else:
+            print("That letter doesn't appear in the word ")
+    print("Thanks for playing!")
+    print("We'll see how well you did in the next stage")
 
 
 if __name__ == "__main__":
