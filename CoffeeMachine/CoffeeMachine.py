43143215174d23e5916c068a059f5d631960e766
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-06 21:50:53 +0200

    CoffeeMachine 3-rd stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,4 +1,8 @@
 class CoffeeMachine:
+    water = 0
+    milk = 0
+    beans = 0
+
     def make_coffee(self):
         print("Starting to make a coffee")
         print("Grinding coffee beans")
@@ -13,3 +17,12 @@ class CoffeeMachine:
         print(f"{200 * num_of_cups} ml of water")
         print(f"{50 * num_of_cups} ml of milk")
         print(f"{15 * num_of_cups} g of coffee beans")
+
+    def available(self, num_of_cups):
+        cups_available = min((int(self.water / 200), int(self.milk / 50), int(self.beans / 15)))
+        if cups_available == num_of_cups:
+            print("Yes, I can make that amount of coffee")
+        elif cups_available > num_of_cups:
+            print(f"Yes, I can make that amount of coffee (and even {cups_available - num_of_cups} more than that)")
+        else:
+            print(f"No, I can make only {cups_available} cups of coffee")
--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -3,9 +3,17 @@ from CoffeeMachine import CoffeeMachine
 
 def main():
     coffee_machine = CoffeeMachine()
+
+    print("Write how many ml of water the coffee machine has:")
+    coffee_machine.water = int(input())
+    print("Write how many ml of milk the coffee machine:")
+    coffee_machine.milk = int(input())
+    print("Write how many grams of coffee beans the coffee machine:")
+    coffee_machine.beans = int(input())
+
     print("Write how many cups of coffee you will need:")
     num_of_cups = int(input())
-    coffee_machine.calc_amount_of_ingredients(num_of_cups)
+    coffee_machine.available(num_of_cups)
 
 
 if __name__ == '__main__':
