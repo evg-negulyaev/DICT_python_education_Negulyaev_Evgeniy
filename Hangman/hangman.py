a0257d4ef843a0840430ec398f6277843f8f870f
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-06 19:43:30 +0200

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,10 @@
 from random import randint
 
 
+def hint(word):
+    return word[0:3] + "-" * (len(word) - 3)
+
+
 def random_word(words):
     return words[randint(0, len(words) - 1)]
 
@@ -10,7 +14,7 @@ def main():
 
     words = ['python', 'java', 'javascript', 'php']
     word_to_guess = random_word(words)
-    print("Guess the word: ", end="")
+    print(f"Guess the word {hint(word_to_guess)}: ", end="")
     word = input()
     if word_to_guess == word:
         print("You survived!")
