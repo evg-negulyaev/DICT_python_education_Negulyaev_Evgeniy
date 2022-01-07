1ba9f8618c98ee7f78fd8073bcdb7fa34909dd25
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-07 12:35:55 +0200

    DinnerParty 3-rd stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def main():
     print("Enter the number of friends joining (including you):")
     num = int(input())
@@ -5,16 +8,24 @@ def main():
         print('No one is joining for the party')
         return
 
-    print("Enter the name of every friend (including you), each on a new line:")
+    print("\nEnter the name of every friend (including you), each on a new line:")
     participants = dict()
     for _ in range(num):
         participants[input()] = 0
 
-    print('Enter the total amount:')
+    print('\nEnter the total amount:')
     amount = int(input())
+
     for name in participants.keys():
         participants[name] = round(amount / num, 2)
-    print(participants)
+
+    print('\nDo you want to use the "Who is lucky?" feature? Write Yes/No:')
+    if input() == 'Yes':
+        names = list(participants.keys())
+        lucky = names[randint(0, len(names) - 1)]
+        print(f'{lucky} is the lucky one!')
+    else:
+        print('No one is going to be lucky')
 
 
 if __name__ == '__main__':
