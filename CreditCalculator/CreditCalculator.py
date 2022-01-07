e0289e0766e34552cf24eb843bb45704e273727f
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-07 08:45:39 +0200

    CreditCalculator 2-nd stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,9 +1,26 @@
+from math import ceil
+
+
 def main():
-    print('Loan principal: 1000')
-    print('Month 1: repaid 250')
-    print('Month 2: repaid 250')
-    print('Month 3: repaid 500')
-    print('The loan has been repaid!')
+    print('Enter the loan principal:')
+    principal = int(input())
+
+    print('What do you want to calculate?')
+    print('type "m" – for number of monthly payments, type "p" – for the monthly payment:')
+    action = input()
+    if action == 'm':
+        print('Enter the monthly payment:')
+        payment = int(input())
+        print(f'It will take {ceil(principal / payment)} months to repay the loan')
+    elif action == 'p':
+        print('Enter the number of months:')
+        periods = int(input())
+        payment = ceil(principal / periods)
+        last_payment = principal - (periods - 1) * payment
+        if payment == last_payment:
+            print(f'Your monthly payment = {payment}')
+        else:
+            print(f'Your monthly payment = {payment} and the last payment = {last_payment}')
 
 
 if __name__ == '__main__':
