b36fbf59f606953f3ee169a468a7ca3533fa0228
evg-negulyaev <negulyaev.evg375@gmail.com>
2022-01-07 09:40:19 +0200

    CreditCalculator 4-th stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,50 +1,31 @@
+import argparse
 from math import ceil, log, trunc
 
 
 def main():
-    print('What do you want to calculate?')
-    print('''type "n" for number of monthly payments, 
-type "a" for annuity monthly payment amount, 
-type "p" for loan principal:''')
-    action = input()
-    if action == 'n':
-        print("Enter the loan principal:")
-        payment = int(input())
-        print("Enter the monthly payment:")
-        month_payment = int(input())
-        print("Enter the loan interest:")
-        loan_int = float(input())
-        i = loan_int / (12 * 100)
-        num = ceil(log(month_payment / (month_payment - i * payment), 1 + i))
-        years = trunc(num / 12)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--type", type=str)
+    parser.add_argument("--principal", type=int)
+    parser.add_argument("--periods", type=int)
+    parser.add_argument("--interest", type=float)
+    args = parser.parse_args()
 
-        months = num - years * 12
-        if months == 0:
-            print(f"It will take {years} years to repay this loan!")
-        elif years == 0:
-            print(f"It will take {months} months to repay this loan!")
-        else:
-            print(f"It will take {years} years and {months} months to repay this loan!")
-    elif action == 'p':
-        print("Enter the annuity payment:")
-        a_payment = float(input())
-        print("Enter the number of periods:")
-        period = int(input())
-        print("Enter the loan interest:")
-        loan_int = float(input())
-        i = loan_int / (12 * 100)
-        loan = trunc(a_payment * ((1 + i) ** period - 1) / (i * (1 + i) ** period))
-        print(f"Your loan principal = {loan}!")
-    elif action == 'a':
-        print("Enter the loan principal:")
-        payment = int(input())
-        print("Enter the number of periods:")
-        period = int(input())
-        print("Enter the loan interest:")
-        loan_int = float(input())
-        i = loan_int / (12 * 100)
-        month_pay = ceil((payment * i * ((1 + i) ** period)) / (((1 + i) ** period) - 1))
-        print(f"Your monthly payment = {month_pay}!")
+    t = args.type
+    principal = args.principal
+    periods = args.periods
+    interest = args.interest
+
+    if t == "diff":
+        inter = interest / (12 * 100)
+        s = 0
+        for n in range(periods):
+            month_pay = ceil(principal / periods + inter * (principal - (principal * ((n + 1) - 1)) / periods))
+            print(f"Month{n+1}: payment is {month_pay}")
+            s += month_pay
+        overpayment = s - principal
+        print(f"Overpayment = {overpayment}")
+    else:
+        print("invalid key")
 
 
 if __name__ == '__main__':
